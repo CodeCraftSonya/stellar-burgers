24efe5574d99ebaa6f84272a8b596eb89d6eebc7
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-05-19 12:08:22 +0300

    feat: add error message in login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,16 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
-import { useDispatch } from '../../services/store';
-import { login } from '../../services/userSlice';
+import { useDispatch, useSelector } from '../../services/store';
+import { login, userSlice } from '../../services/userSlice';
+
+const { selectUserIsError } = userSlice.selectors;
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
+  const isError = useSelector(selectUserIsError);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
@@ -17,7 +20,7 @@ export const Login: FC = () => {
 
   return (
     <LoginUI
-      errorText=''
+      errorText={isError ? 'Неверный логин или пароль' : ''}
       email={email}
       setEmail={setEmail}
       password={password}
